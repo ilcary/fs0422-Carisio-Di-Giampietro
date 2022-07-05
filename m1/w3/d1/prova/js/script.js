ff44933a5293ce0c8dc66dd4727141bd170ed57b
ilCary <carisiodigiampietro54@gmail.com>
2022-07-05 11:20:18 +0200

    java script

--- m1/w3/d1/prova/js/script.js
+++ m1/w3/d1/prova/js/script.js
@@ -1,16 +1,17 @@
 let display = document.getElementById('display');
 
-let buttons = Array.from(document.getElementsByClassName('button'));
+let buttons = (document.querySelectorAll('.button'));
 
-buttons.map( button => {
+buttons.forEach( button => {
     button.addEventListener('click', (e) => {
         switch(e.target.innerText){
             case 'C':
                 display.innerText = '';
                 break;
+
             case '=':
                 try{
-                    display.innerText = eval(display.innerText);
+                    display.innerText = parseint(display.innerText);
                 } catch {
                     display.innerText = "Error"
                 }
@@ -20,6 +21,7 @@ buttons.map( button => {
                    display.innerText = display.innerText.slice(0, -1);
                 }
                 break;
+
             default:
                 display.innerText += e.target.innerText;
         }
