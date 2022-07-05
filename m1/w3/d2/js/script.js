6ff361c373702e8858ecbc58217d3f244dcece42
ilCary <carisiodigiampietro54@gmail.com>
2022-07-05 13:52:57 +0200

    patch cartelle

---
+++ m1/w3/d2/js/script.js
@@ -0,0 +1,61 @@
+let nome = "Mirtilla";
+console.log(nome);
+var utente = "cliente";
+console.log(utente);
+const PI = 3.14;
+console.log(PI);
+
+//nome = "Lilla";
+console.log(nome);
+utente = "amministratore";
+console.log(utente);
+//PI = 4;
+//console.log(PI);
+
+let primoLavoro = "developing";
+let secondoLavoro = "formazione";
+
+let lavoro = primoLavoro + " e " + secondoLavoro;
+
+console.log('io mi occupo di: ' + lavoro);
+console.log('io mi occupo di: ' + primoLavoro + " e " + secondoLavoro);
+
+let JS = true;
+console.log('Let JS: ' + JS);
+
+let anno;
+console.log(anno);
+
+anno = 1991;
+
+const annoAttuale = 2021;
+
+let anniMirtilla = annoAttuale - anno;
+console.log(anniMirtilla);
+
+console.log(nome + " ha: " + anniMirtilla + " anni");
+
+let a = 10 + 5;
+let b = 3;
+let c = 10;
+let somma = b + c + 2;
+console.log(somma);
+
+c++;
+console.log(c);
+a--;
+console.log(a);
+
+let nomePet = "Billo";
+let colorePet = "rosso";
+const pet = "il nome del pet: " + nomePet + "e il suo colore è: " + colorePet;
+console.log(pet);
+
+// sintassi letterale con backtick -> apice inverso
+const pet1 = `il nome del pet: ${nomePet}  e il suo colore è: ${colorePet}`;
+console.log(pet1);
+
+console.log('string con \n\ righe \n\ multiple');
+console.log(`string con 
+righe 
+multiple`);
