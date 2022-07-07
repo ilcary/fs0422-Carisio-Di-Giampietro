bee0731ee0eb5702c022dc5314a91c7328984c66
ilCary <carisiodigiampietro54@gmail.com>
2022-07-07 17:39:03 +0200

    calcolatrice

--- m1/w3/d5/calcolatrice/js/script.js
+++ m1/w3/d5/calcolatrice/js/script.js
@@ -1,29 +1,63 @@
-let display = document.getElementById('display');
+//selector
+const output = document.querySelector(".output");
+const result = document.querySelector(".result");
+const keys = document.querySelectorAll("button");
 
-let buttons = (document.querySelectorAll('.button'));
-
-buttons.forEach( button => {
-    button.addEventListener('click', (e) => {
-        switch(e.target.innerText){
-            case 'C':
-                display.innerText = '';
-                break;
-
-            case '=':
-                try{
-                    display.innerText = eval(display.innerText);
-                } catch {
-                    display.innerText = "Error"
-                }
-                break;
-            case '←':
-                if (display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);
-                }
-                break;
-
-            default:
-                display.innerText += e.target.innerText;
-        }
-    });
+//eventlistener
+keys.forEach(key => {
+    key.addEventListener("click", calculate);
 });
+
+
+
+
+
+function calculate() {
+    let buttonText = this.innerText;
+    if (buttonText === "AC") {
+        output.innerText = "";
+        result.innerText = "0";
+        result.style.animation = "";
+        output.style.animation = "";
+        return;
+    }
+
+    if (buttonText === "DEL") {
+        output.textContent = output.textContent.substr(0, output.textContent.length - 1);
+        return;
+    }
+
+    if (buttonText === "=") {
+        result.innerText = eval(output.innerText);
+        result.style.animation = "big 0.5s ease-in-out";
+        output.style.animation = "small 0.5s ease-in-out";
+        result.style.animationFillMode = "forwards";
+        output.style.animationFillMode = "forwards";
+    }
+
+
+    if (buttonText === 'x') {
+        output.innerText += '*'
+    }
+
+    else {
+        output.textContent += buttonText;
+        return;
+    }
+
+
+}
+
+
+
+$('#make3d').change(function(){
+    if($(this).is(":checked")) {
+        $('.calculator').addClass("setted3d");
+        $('button').addClass('setted3dbutton');
+    } else {
+        $('.calculator').removeClass("setted3d");
+        $('button').removeClass('setted3dbutton');
+    }
+});
+
+
