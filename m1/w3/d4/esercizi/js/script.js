7cf500822b0721014d1c1363faedc59208d4c921
ilCary <carisiodigiampietro54@gmail.com>
2022-07-07 14:18:23 +0200

    home work

---
+++ m1/w3/d4/esercizi/js/script.js
@@ -0,0 +1,31 @@
+
+function acronimo(text) {
+    return text
+      .split(' ')
+      .reduce(function(accumulator, word) {
+        return accumulator + word.charAt(0);
+      }, '');
+  }
+  
+  console.log(acronimo('Mario Rossi'));
+
+
+
+let persone = ['Silvio', 'Alessio', 'Mirko', 'Alessandro']
+
+console.log(persone)   
+console.log(persone[2])
+console.log(persone.length)
+
+persone.push('Davide', 'Giuliana')// aggiungo con .push Davide e Giuliana
+
+console.log(persone) 
+
+persone.pop()// Davide si lascia con Giuliana quindi la levo dalle persone con cui potrei uscire con il metodo .pop
+
+console.log(persone) 
+
+let scelta = Math.floor(Math.random() * persone.length)// scelgo una persona a caso con cui uscire
+
+console.log(`Questa sera esco con: ${persone[scelta]}`)
+   
