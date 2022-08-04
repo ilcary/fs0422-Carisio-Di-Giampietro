3715a1ca1b225ae5848ae04279702c7022c4e5e6
ilCary <carisiodigiampietro54@gmail.com>
2022-08-04 14:31:32 +0200

    nodeeee

--- m2/w3/d4/crud/js/delete.js
+++ m2/w3/d4/crud/js/delete.js
@@ -1,26 +1,10 @@
-function eliminaUtente(id,el){
+function eliminaUtente(id) {
     const apiUtenti = 'http://localhost:3000/utenti';
+    fetch(apiUtenti + '/' + id, {
 
-    let options = {
-        method: 'DELETE',
-        headers:{
-            "content-type":"application/json"
-        }
-    }
-
-    fetch(apiUtenti+'/'+id,options)
-    .then(res => res.json())
-    .then(res => {
-      
-        el.remove()
-        
-        Swal.fire({
-            position: 'top-end',
-            icon: 'success',
-            title: 'Utente Eliminato',
-            text:`L'utente è stato eliminato`,
-            showConfirmButton: false,
-            timer: 5000
-        })
     })
+        .then(response => response.json())
+        .then(data => {
+
+        })
 }
