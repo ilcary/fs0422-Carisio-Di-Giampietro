53098bde7b760837d82328e237e0c81c74cfcc1e
ilCary <carisiodigiampietro54@gmail.com>
2022-08-30 12:56:43 +0200

    lesson 30/08/2022

--- m3/w1/d2/todo-oop-ts/script.js
+++ m3/w1/d2/todo-oop-ts/script.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 class Todo {
     constructor(selettore) {
         this.target = document.querySelector(selettore);
@@ -6,41 +7,53 @@ class Todo {
         let db = localStorage.getItem(selettore) || '[]';
         this.allTodos = JSON.parse(db);
         this.creaHTMLBase();
+        if (this.allTodos.length > 0) {
+            for (let todo of this.allTodos) {
+                this.creaHTMLTodo(todo);
+            }
+        }
     }
     creaHTMLBase() {
         //creo gli elementi
-        let input = document.createElement("input");
-        let btn = document.createElement("button");
-        let list = document.createElement("div");
-        //attivo funzionalità button
-        btn.addEventListener("click", () => {
+        let input = document.createElement('input');
+        let button = document.createElement('button');
+        let list = document.createElement('div');
+        //attivo funzionalità del button
+        button.addEventListener('click', () => {
             this.creaHTMLTodo();
             this.salvaNuovoTodo();
         });
         //formatto gli elementi
-        input.classList.add("form-control");
-        btn.classList.add("btn", "btn-primary");
-        btn.textContent = "salva";
-        //salvo gli elementi nelle proprieta della classe
+        input.classList.add('form-control');
+        button.classList.add('btn', 'btn-primary');
+        button.textContent = 'Salva';
+        //salvo gli elementi nelle proprietà della classe
         this.input = input;
-        this.btn = btn;
+        this.button = button;
+        this.list = list;
+        //mostro gli elementi
         if (this.target) {
-            this.target.append(input, btn, list);
+            this.target.append(input, button, list);
         }
         else {
-            throw new Error("No target specified");
+            throw new Error(`Elemento target(${this.selettore}) non trovato`);
         }
     }
     creaHTMLTodo(testo = null) {
-        let newTodo = document.createElement("div");
-        newTodo.classList.add("alert", 'alert-success');
+        let newTodo = document.createElement('div');
+        newTodo.classList.add('alert', 'alert-success');
         newTodo.textContent = testo || this.input.value;
         this.list.append(newTodo);
     }
     salvaNuovoTodo() {
         this.allTodos.push(this.input.value);
         localStorage.setItem(this.selettore, JSON.stringify(this.allTodos));
+        this.input.value = '';
     }
 }
-new Todo('#todo');
+let todo1 = new Todo('#todo');
+let todo2 = new Todo('#todo2');
+(_a = document.querySelector('#btnProva')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
+    todo1.creaHTMLTodo('prova');
+});
 //# sourceMappingURL=script.js.map
--- m3/w1/d2/todo-oop-ts/script.ts
+++ m3/w1/d2/todo-oop-ts/script.ts
@@ -1,58 +1,71 @@
-class Todo {
-    protected target!: HTMLElement | null;
-    protected selettore!:string;
-    protected input!: HTMLInputElement | null;
-    protected btn!: HTMLButtonElement | null;
-    protected list!: HTMLElement | null;
-    protected allTodos!:string[];
 
-    constructor(selettore: string) {
+class Todo{
+    protected target!:HTMLElement|null;
+    protected selettore!:string;
+    protected input!:HTMLInputElement;
+    protected button!:HTMLButtonElement;
+    protected list!:HTMLElement;
+    protected allTodos!:string[];
+    
+    constructor(selettore:string){
         this.target = document.querySelector(selettore);
         this.selettore = selettore;
         let db:string = localStorage.getItem(selettore) || '[]';
         this.allTodos = JSON.parse(db);
-        this.creaHTMLBase()
+        this.creaHTMLBase();
 
-    }
-
-    protected creaHTMLBase() {
-        //creo gli elementi
-        let input: HTMLInputElement = document.createElement("input");
-        let btn: HTMLButtonElement = document.createElement("button");
-        let list: HTMLElement = document.createElement("div");
-        //attivo funzionalità button
-        btn.addEventListener("click", () => {
-            this.creaHTMLTodo();
-            this.salvaNuovoTodo();
-        })
-
-        //formatto gli elementi
-        input.classList.add("form-control");
-        btn.classList.add("btn", "btn-primary");
-        btn.textContent = "salva";
-
-        //salvo gli elementi nelle proprieta della classe
-        this.input = input;
-        this.btn = btn;
-
-        if (this.target) {
-            this.target.append(input, btn, list)
-        } else {
-            throw new Error("No target specified")
+        if(this.allTodos.length > 0){
+            for(let todo of this.allTodos){
+                this.creaHTMLTodo(todo)
+            }
         }
     }
 
-    public creaHTMLTodo(testo:string|null = null) {
-        let newTodo = document.createElement("div");
-        newTodo.classList.add("alert", 'alert-success');
+    protected creaHTMLBase(){
+        //creo gli elementi
+        let input:HTMLInputElement = document.createElement('input');
+        let button:HTMLButtonElement = document.createElement('button');
+        let list:HTMLElement = document.createElement('div');
+        //attivo funzionalità del button
+        button.addEventListener('click',() => {
+            this.creaHTMLTodo();
+            this.salvaNuovoTodo();
+        })
+        //formatto gli elementi
+        input.classList.add('form-control');
+        button.classList.add('btn','btn-primary');
+        button.textContent = 'Salva';
+        //salvo gli elementi nelle proprietà della classe
+        this.input = input;
+        this.button = button;
+        this.list = list;
+        //mostro gli elementi
+        if(this.target){
+           this.target.append(input, button, list)
+        }else{
+            throw new Error(`Elemento target(${this.selettore}) non trovato`)
+        }
+    
+    }
+
+    public creaHTMLTodo(testo:string|null = null){
+        let newTodo = document.createElement('div');
+        newTodo.classList.add('alert','alert-success')
         newTodo.textContent = testo || this.input.value;
         this.list.append(newTodo);
     }
 
     protected salvaNuovoTodo(){
-        this.allTodos.push(this.input.value);
-        localStorage.setItem(this.selettore, JSON.stringify(this.allTodos));
+        this.allTodos.push(this.input.value)
+        localStorage.setItem(this.selettore,JSON.stringify(this.allTodos))
+        this.input.value = '';
     }
 }
 
-new Todo('#todo')
+let todo1 = new Todo('#todo')
+let todo2 = new Todo('#todo2')
+
+document.querySelector('#btnProva')
+?.addEventListener('click',function(){
+    todo1.creaHTMLTodo('prova')
+})
