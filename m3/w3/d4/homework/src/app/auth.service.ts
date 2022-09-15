7b1376c1574385b4bd273adb29c7e6893b1c74eb
ilCary <carisiodigiampietro54@gmail.com>
2022-09-15 17:08:23 +0200

    hw +lesson

---
+++ m3/w3/d4/homework/src/app/auth.service.ts
@@ -0,0 +1,65 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+export interface ILogin {
+  name: string,
+  email: string,
+  password: string
+}
+
+export interface IRegister {
+  email: string,
+  password: string
+}
+
+
+type AuthResponse = {
+  accessToken: string,
+  user: {
+    name?:string,
+    email: string,
+    id: number
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http:HttpClient) { }
+
+  apiUrl:string = 'http://localhost:3000'
+
+  register(registerData:IRegister){
+    return this.http.post<AuthResponse>(this.apiUrl+'/register', registerData)
+  }
+
+  login(loginData:ILogin){
+    return this.http.post<AuthResponse>(this.apiUrl+'/login', loginData)
+  }
+
+  isUserLogged():boolean{
+    return localStorage.getItem('access') != null
+  }
+
+  getLoggedUser(){
+    let db = localStorage.getItem('access')
+    return db ? JSON.parse(db).user : null
+  }
+  getAccessToken(){
+    let db = localStorage.getItem('access')
+    console.log(db);
+
+    return db ? JSON.parse(db).accessToken : null
+  }
+
+  saveAccessData(data:AuthResponse){
+    localStorage.setItem('access',JSON.stringify(data))
+  }
+
+  logOut(){
+    localStorage.removeItem('access')
+  }
+
+}
