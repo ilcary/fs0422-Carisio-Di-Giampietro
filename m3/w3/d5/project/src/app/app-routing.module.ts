88fb8900460cf5ec6380f7c16ba7cb02f45f7f00
ilCary <carisiodigiampietro54@gmail.com>
2022-09-19 08:59:30 +0200

    patch guard

--- m3/w3/d5/project/src/app/app-routing.module.ts
+++ m3/w3/d5/project/src/app/app-routing.module.ts
@@ -6,11 +6,11 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
   { path: 'users', loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule), canActivate:[GuardLogGuard] },
-  { path: 'posts', loadChildren: () => import('./pages/posts/posts.module').then(m => m.PostsModule), canActivate:[GuardLogGuard]},
+  { path: 'posts', loadChildren: () => import('./pages/posts/posts.module').then(m => m.PostsModule), canActivate:[GuardLogGuard] },
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
   { path: 'signup', loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupModule) },
-  { path: 'inactivePost', loadChildren: () => import('./pages/inactive-post/inactive-post.module').then(m => m.InactivePostModule) },
-  { path: 'users/editUser/:id', loadChildren: () => import('./pages/users/edit-user/edit-user.module').then(m => m.EditUserModule) },
+  { path: 'inactivePost', loadChildren: () => import('./pages/inactive-post/inactive-post.module').then(m => m.InactivePostModule),canActivate:[GuardLogGuard] },
+  { path: 'users/editUser/:id', loadChildren: () => import('./pages/users/edit-user/edit-user.module').then(m => m.EditUserModule),canActivate:[GuardLogGuard] },
 ];
 
 @NgModule({
